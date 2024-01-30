0acc02e89995b03e3fda265d87befa6aa08a2092
Daniela <daniela.galvis.jaimes@gmail.com>
2024-01-29 22:10:10 -0500

    agregar retos

---
+++ reto1.php
@@ -0,0 +1,45 @@
+<?php
+
+// Suma
+$edad=33;
+$altura=1.50;
+
+$resultado1=$edad+$altura;
+echo "<br>";
+echo "$resultado1";
+echo "<br>";
+
+// Resta
+$resultado2=$edad-$altura;
+echo "<br>";
+echo "$resultado2";
+echo "<br>";
+
+
+//Multiplicaciòn
+
+$resultado3=$edad*$altura;
+echo "<br>";
+echo "$resultado3";
+echo "<br>";
+
+//Divisiòn
+
+$resultado4=$edad/$altura;
+echo "<br>";
+echo "$resultado4";
+echo "<br>";
+
+// Potencia
+
+$resultado5=$edad**$altura;
+echo "<br>";
+echo "$resultado5";
+echo "<br>";
+
+//Raiz
+
+$resultado6=sqrt($edad);
+echo "<br>";
+echo "$resultado6";
+echo "<br>";
